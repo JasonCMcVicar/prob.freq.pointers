d320334ca6c003fe9d02a97fee35eb423f424061
Joel <jcbluemel@yahoo.com>
2022-05-27 15:50:34 -0700

    completed averagePair func

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,18 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+"use strict";
+
+/** Return true/false if targetAvg can be made with 2 nums in array nums */
+
+function averagePair(nums, targetAvg) {
+  let left = 0;
+  let right = nums.length-1;
+
+  while (left < right) {
+    const avg = (nums[left] + nums[right]) / 2;
+
+    if (avg === targetAvg) return true;
+    else if (avg > targetAvg) right--;
+    else left++;
+  }
+  return false;
 }
 
