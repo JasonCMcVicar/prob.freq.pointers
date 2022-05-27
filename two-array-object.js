7c21b084df1e9dd66024fe301ff7c79c17d803f0
Joel <jcbluemel@yahoo.com>
2022-05-27 16:05:03 -0700

    completed twoArrayObject func

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,19 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+/** Return obj with key-value pairs from two different arrays
+ *  based on shared index. Ignored extra values, if extra keys,
+ *  set value to null.
+*/
+
+function twoArrayObject(keys, values) {
+  const keysToValues = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    if (values.length <= i) {
+      keysToValues[keys[i]] = null;
+    } else {
+      keysToValues[keys[i]] = values[i];
+    }
+  }
+  return keysToValues;
 }
