a0687b08c8ea8beb0856dcdd775873f412e1131e
Jason <jasoncmcvicar@gmail.com>
2022-05-27 20:02:37 -0500

    solution for separate positive challenge

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,21 @@
-// add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+"use strict";
+/** take an array of numbers and mutate it by placing positive numbers to the right and
+ * negative numbers to the left. return the array;
+ */
+function separatePositive(nums) {
+
+  let begin = 0;
+  let end = nums.length - 1;
+
+  while (begin < end) {
+    if (nums[begin] > 0) {
+      begin++;
+    } else if (nums[end] < 0) {
+      end--;
+    } else {
+      [nums[begin], nums[end]] = [nums[end], nums[begin]];
+      //[nums[left], nums[right]] = [nums[right], nums[left]];
+    }
+  }
+  return nums;
 }
